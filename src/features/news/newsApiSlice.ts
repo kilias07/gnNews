fdf8ded768df38df8daf5e9ceaddcaf674201368
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2023-03-30 23:18:49 +0200

    add proper .env file

--- src/features/news/newsApiSlice.ts
+++ src/features/news/newsApiSlice.ts
@@ -1,17 +1,17 @@
-import { Article, News, Country } from "./../../types/types";
+import { News, Country } from "./../../types/types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 type Args = {
   limit?: number;
   countryCode?: Country["code"];
 };
-const NEWS_API_KEY = "309e3ec0607e4158a447981244852e63";
+
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://newsapi.org/v2/",
     prepareHeaders(headers) {
-      headers.set("x-api-key", NEWS_API_KEY);
+      headers.set("x-api-key", import.meta.env.VITE_NEWS_API_KEY);
 
       return headers;
     },
